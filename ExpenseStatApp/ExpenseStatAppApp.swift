57900f2bdf329deb96e53173ca1d23796b0b56e0
LopezMat <matthieulopezpro66@gmail.com>
2025-04-16 09:49:48 +0200

    Initial Commit

---
+++ ExpenseStatApp/ExpenseStatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseStatAppApp.swift
+//  ExpenseStatApp
+//
+//  Created by Stagiaire on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseStatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
